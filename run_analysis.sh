5b048301256e56abbeb2d0d9113e303e40a00d7e
Aurelio Amerio <aure.amerio@gmail.com>
2023-09-07 09:44:06 +0200

    up

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-root="/home/aure/Github/fermi-scripts/test"
-dirname="test"
+root="/home/aure/Github/fermitools-scripts/test_run"
+dirname="test_run"
 weak_in=9
 weak_out=15 #795
 Emin=1000
 Emax=10000
 nenergies=10
 healpixorder=3 #11
+cleanup=true
 
 python download_fermi_data.py -r $root --weak_in $weak_in --weak_out $weak_out # download data
 python make_bin_txt.py -r $root --Emin $Emin --Emax $Emax -n $nenergies # make binning file
@@ -25,3 +26,11 @@ python make_selection_txt.py -r $root --weak_in $weak_in --weak_out $weak_out # 
 ./gtltcube.sh $root $dirname
 ./gtexpcube2.sh $root $dirname $healpixorder
 ./gtpsf.sh $root $dirname $Emin $Emax
+
+# cleanup
+if $cleanup then
+	echo "Performing cleanup"
+    rm -r *.par
+fi
+
+echo "Done!"
